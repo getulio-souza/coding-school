232fed06995100db1a77fae35174fe9ffc9f4b33
getulio.souza <getulio.souza@evolutix.com.br>
2023-04-15 19:16:08 -0300

    feature: design-token-created

--- src/shared/DesignTokens.js
+++ src/shared/DesignTokens.js
@@ -1,9 +1,55 @@
-import React from 'react'
-
-function DesignTokens() {
-  return (
-    <div>DesignTokens</div>
-  )
+export const Colors = {
+    NEUTRAL_WHITE: '#FFFFF',
+    NEUTRAL_BLACK: '#00000',
+    DARK_PURPLE: '#390072'
 }
 
-export default DesignTokens
+export const FontFamilies = {
+    PRIMARY: 'inter',
+};
+
+export const FontLetterSpacings = {
+    SMALL: '0.5px',
+    MEDIUM: '0.15PX',
+    LARGE: '1.25px',
+};
+
+export const FontLineHeights = {
+    SMALL: '120%',
+    MEDIUM: '150%',
+};
+
+export const FontSizes = {
+    ONE: '8px',
+    ONE_HALF: '12px',
+    ONE_QUARTER: '14px',
+    TWO: '16px',
+    THREE: '24px',
+    FOUR: '32px',
+};
+
+export const FontWeights = {
+    THIN: 300,
+    REGULAR: 400,
+    BOLD: 500,
+};
+
+export const Spaces = {
+    NONE: '0',
+    HALF: '4px',
+    ONE: '8px',
+    ONE_HALF: '12px',
+    TWO: '16px',
+    THREE: '24px',
+    FOUR: '32px',
+    FIVE: '40px',
+    SIX: '48px',
+    SEVEN: '56px',
+    EIGHT: '64px',
+    NINE: '72px',
+    TEN: '88px',
+};
+
+export const BorderRadius = {
+    BUTTON_BORDER: '12px',
+}
